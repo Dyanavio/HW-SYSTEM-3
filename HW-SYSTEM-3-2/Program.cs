79432c878440112516db8afb5a016c795deac715
Dyanavio <dimant2018@gmail.com>
2025-01-23 12:09:07 +0300

    Add project files.

---
+++ HW-SYSTEM-3-2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HW_SYSTEM_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- Fibonacci numbers -----");
+            Thread thread = new(Fibonacci);
+            thread.Start();
+
+            Console.ReadKey();
+
+        }
+        static void Fibonacci()
+        {
+            List<long> list = new List<long>() { 0, 1 };
+            Console.WriteLine($"{list[0]}\n{list[1]}");
+            for(int i = 0; ;i++)
+            {
+                list.Add(list[i] + list[i + 1]);
+                Console.WriteLine(list.Last());
+                Thread.Sleep(300);
+            }
+
+        }
+    }
+}
---
+++ HW-SYSTEM-3/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace HW_SYSTEM_3
+{
+    internal class Program
+    {
+        static int? lower = null;
+        static int? upper = null;
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the lower and the upper boundaries");
+                Console.Write("Lower: ");
+                lower = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Upper (0 for infinity): ");
+                upper = Convert.ToInt32(Console.ReadLine());
+
+                if (lower < 0 || upper < 0) throw new Exception("Boundaries can't be negative");
+
+                Thread thread = new Thread(OutputAllPrimes);
+                thread.Start();
+               
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ResetColor();
+            }
+        }
+        static void OutputAllPrimes()
+        {
+            if (lower == null) lower = 2;
+            if (upper != 0)
+            {
+                for(int? i = lower; i < upper; i++)
+                {
+                    if (IsPrime(i)) Console.WriteLine(i);
+                    Thread.Sleep(200);
+                }
+            }
+            else
+            {
+                for(int? i = lower; ;i++)
+                {
+                    if (IsPrime(i)) Console.WriteLine(i);
+                    Thread.Sleep(200);
+                }
+            }
+        }
+        static bool IsPrime(int? number)
+        {
+            bool flag = true;
+            for (int j = 2; j < number; j++)
+            {
+                if (number % j == 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+        }
+    }
+}
